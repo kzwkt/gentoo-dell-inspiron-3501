3a38a6c4b6533e7e8aed1642daede0e3a61d9005
kzwkt <9437935+kzwkt@users.noreply.github.com>
2023-09-29 14:08:07 +0545

    Update wmn.sh

--- .bin/wmn.sh
+++ .bin/wmn.sh
@@ -1,7 +1,7 @@
 drives="$(lsblk -npro "name,size,mountpoint,fstype,label"|sed "s/ /:/g" | awk -F':' '$3=="" &&  $4!="" {printf "%s %s %s %s %s\n",$1,$2,$3,$4,$5}')"
-sele="$(echo "$drives" | wofi --dmenu | awk '{print $1}')"
+sele="$(echo "$drives" | wofi -i --dmenu -p "mount which drive?" --width 20% --height=25% | awk '{print $1}')"
 set -e
 test -n "$sele"
-mp="$(find /home/kai/mnt -maxdepth 1 -type d 2>/dev/null | wofi --dmenu)"
+mp="$(find /home/kai/mnt -maxdepth 1 -type d 2>/dev/null | wofi -i --dmenu -p "mount in" --width 25% --height=20% )"
 test -n "$mp"
 sudo -A mount "$sele" $mp

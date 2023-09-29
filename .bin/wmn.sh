fc20a83427db22eb82a708f82548a0c4a4834fca
kzwkt <9437935+kzwkt@users.noreply.github.com>
2023-09-29 13:51:29 +0545

    Create wmn.sh

---
+++ .bin/wmn.sh
@@ -0,0 +1,11 @@
+drives="$(lsblk -npro "name,size,mountpoint,fstype,label"|sed "s/ /:/g" | awk -F':' '$3=="" &&  $4!="" {printf "%s %s %s %s %s\n",$1,$2,$3,$4,$5}')"
+sele="$(echo "$drives" | wofi --dmenu | awk '{print $1}')"
+set -e
+test -n "$sele"
+mp="$(find /home/kai/mnt -maxdepth 1 -type d 2>/dev/null | wofi --dmenu)"
+test -n "$mp"
+if [ ! -d "$mp" ]; then
+mkdiryn=$(printf "No\\nYes" | wofi --dmenu)
+[ "$mkdiryn" = "Yes" ] && (mkdir -p "$mp")
+fi
+sudo -A mount "$sele" $mp

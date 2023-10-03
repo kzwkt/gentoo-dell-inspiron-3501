50f27d45df1a116824bae14a838b3a06014a4479
kzwkt <9437935+kzwkt@users.noreply.github.com>
2023-10-03 17:01:46 +0000

    Add files via upload

--- .bin/wmn.sh
+++ .bin/wmn.sh
@@ -1,4 +1,4 @@
-drives="$(lsblk -npro "name,size,mountpoint,fstype,label"|sed "s/ /:/g" | awk -F':' '$3=="" &&  $4!="" {printf "%s %s %s %s %s\n",$1,$2,$3,$4,$5}')"
+drives="$(lsblk -npro "name,size,mountpoint,fstype,label"|sed "s/ /:/g" | awk -F: '$3=="" &&  $4!="" {printf "%s %s %s %s %s\n",$1,$2,$3,$4,$5}')"
 sele="$(echo "$drives" | wofi -i --dmenu -p "mount which drive?" --width 20% --height=25% | awk '{print $1}')"
 set -e
 test -n "$sele"
